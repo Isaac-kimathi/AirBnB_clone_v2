ff8b5b698e981e1f749c978c5799c4e4d145204d
Kimathi isaac <Isaackimathi@gmail.com>
2024-02-01 18:12:16 +0300

    AirBnB_clone_v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-# Bash script that sets up your web servers for the deployment of web_static
+#!/usr/bin/env bash
+# Bash script that sets up web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
@@ -11,12 +11,12 @@ sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
- <head>
- </head>
- <body>
- Alx Africa
- </body>
- </html>" | sudo tee /data/web_static/releases/test/index.html
+  <head>
+    </head>
+      <body>
+          Holberton School
+	</body>
+	</html>" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
